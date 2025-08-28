c04751ada749e3ac668c294ce69195c0890b4e5c
Sushil Mall <sushilmall907@gmail.com>
2025-08-28 23:54:55 +0530

    Wip

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ import reportWebVitals from './reportWebVitals';
 // 1. Import BrowserRouter
 import { BrowserRouter } from 'react-router-dom';
 
+// Force canonical domain in production to avoid OAuth origin mismatch
+
+console.log({HostName:window.location.hostname,Env:process.env.REACT_APP_NODE_ENV});
+
+if (
+  process.env.REACT_APP_NODE_ENV === 'production' &&
+  typeof window !== 'undefined' &&
+  window.location.hostname === 'keepmyasset.com'
+) {
+  const { pathname, search, hash } = window.location;
+  window.location.replace(`https://www.keepmyasset.com${pathname}${search}${hash}`);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
